b4dca74050f0088e6a264f3a1992af50df610708
faffunix-boop <faffunix@gmail.com>
2026-07-19 23:30:14 +0800

    Update router.js

--- backend/pipeline/router.js
+++ backend/pipeline/router.js
@@ -1,1 +1,13 @@
+const logger = require("../utils/logger");
 
+async function router(input) {
+  logger.info("Router", "Detecting task...");
+
+  return {
+    task: "code",
+    model: "qwen/qwen3-30b-a3b",
+    input
+  };
+}
+
+module.exports = router;
