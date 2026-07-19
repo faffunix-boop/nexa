7bf7acaade3aa67f141ad76facead5f0fecba58f
faffunix-boop <faffunix@gmail.com>
2026-07-19 23:31:26 +0800

    Update reviewer.js

--- backend/pipeline/reviewer.js
+++ backend/pipeline/reviewer.js
@@ -1,1 +1,15 @@
+const logger = require("../utils/logger");
 
+async function reviewer(data) {
+  logger.info("Reviewer", "Reviewing response...");
+
+  return {
+    ...data,
+    review: {
+      passed: true,
+      issues: []
+    }
+  };
+}
+
+module.exports = reviewer;
