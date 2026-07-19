7c2136a9d2620e7522e3185dae9ef1d50d588820
faffunix-boop <faffunix@gmail.com>
2026-07-19 23:31:48 +0800

    Update validator.js

--- backend/pipeline/validator.js
+++ backend/pipeline/validator.js
@@ -1,1 +1,12 @@
+const logger = require("../utils/logger");
 
+async function validator(data) {
+  logger.info("Validator", "Validating output...");
+
+  return {
+    ...data,
+    valid: true
+  };
+}
+
+module.exports = validator;
